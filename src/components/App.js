3654debbc739c6eddf68a7e2f38e9b61d987cbd0
Sanusi Olayinka <sanusiolayinka7@gmail.com>
2025-08-28 20:37:50 +0100

    Created the quiz app with useReducer

---
+++ src/components/App.js
@@ -0,0 +1,81 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "Error",
+      };
+
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.question.at(state.index)
+
+      return {
+        ...state,
+        answer: action.payload,
+        points: 
+      };
+
+    default:
+      throw new Error(`Action Unknown`);
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main className="main">
+        {status === "loading" && <Loader />}
+
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            dispatch={dispatch}
+            answer={answer}
+            question={questions[index]}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
---
+++ src/components/Options.js
@@ -0,0 +1,26 @@
+function Options({ quest, dispatch, answer }) {
+  const { question, options, points, correctOption } = quest;
+
+  const isAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            isAnswered && index === correctOption ? "correct" : "wrong"
+          }`}
+          key={option}
+          disabled={isAnswered ? true : false}
+          onClick={() => {
+            dispatch({ type: "newAnswer", payload: index });
+          }}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
